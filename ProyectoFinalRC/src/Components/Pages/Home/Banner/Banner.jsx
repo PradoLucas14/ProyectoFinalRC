b5ae39454e4eefe8a0d5ae8baf68b2ad0be2eb0c
Prado Lucas Nahuel <lukasnahuelprado32@gmail.com>
2024-06-12 08:38:40 -0300

    se creo y se le dio estilos al apartado About que hace la funcion de tarjeta de presentacion para Andinos

--- ProyectoFinalRC/src/Components/Pages/Home/Banner/Banner.jsx
+++ ProyectoFinalRC/src/Components/Pages/Home/Banner/Banner.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import "./Banner.css"
+import { motion } from 'framer-motion';
+
 
 function Banner() {
   return (
     <div className='Banner'>
         <div className='OverlayBanner'>
-            <div className="colum columOne">
-                <div className="tituloContainer">
-                    <h1>ANDINOS</h1>
-                </div>
-                <div className='textContainer'>
-                    <p>
-                        "La Esencia de los Andes en Tu Mesa: 
-                        Reserva Fácilmente con Andinos y Déjate 
-                        Sorprender por Nuestros Sabores"
-                    </p>
-                </div>
-                <div className='btnContainer'>
-                    <button>
-                        <p>Reserva tu mesa</p>
-                    </button>
-                </div>
+            <div className="colum columOne"
+            >
+            <div className="tituloContainer">
+                <h1>ANDINOS</h1>
+            </div>
+            <div className='textContainer'>
+                <p>
+                    "La Esencia de los Andes en Tu Mesa: 
+                    Reserva Fácilmente con Andinos y Déjate 
+                    Sorprender por Nuestros Sabores"
+                </p>
+            </div>
+            <div className='btnContainer'>
+                <button>
+                    <p>Reserva tu mesa</p>
+                </button>
+            </div>
             </div>
             <div className="colum columTwo">
             </div>
--- ProyectoFinalRC/src/Components/Pages/Home/HomePage/HomePage.jsx
+++ ProyectoFinalRC/src/Components/Pages/Home/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Banner from '../Banner/Banner'
 import "./HomePage.css"
+import About from '../About/About'
 
 function HomePage() {
   return (
     <div className='HomePage'>
       <Banner/>
+      <About/>
     </div>
   )
 }
