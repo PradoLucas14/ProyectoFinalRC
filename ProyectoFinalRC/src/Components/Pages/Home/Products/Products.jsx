82bf41c1b7bcce7b5e2b4bc576c95d50830adb0d
Prado Lucas Nahuel <lukasnahuelprado32@gmail.com>
2024-06-14 00:36:07 -0300

    se creo y se le dio estilos a los componentes referidos al apartado top de platillos de la pagina home

--- ProyectoFinalRC/src/Components/Pages/Home/Products/Products.jsx
+++ ProyectoFinalRC/src/Components/Pages/Home/Products/Products.jsx
@@ -1,38 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
+import MenuItem from './MenuItem/MenuItem';
+import "./Products.css"
 
-function ComponentName() {
+const ComponentName = () => {
+    const [menuItems, setMenuItems] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/menu') // Ajusta la URL según donde esté corriendo tu json-server
+            .then(response => response.json())
+            .then(data => setMenuItems(data))
+            .catch(error => console.error('Error fetching menu items:', error));
+    }, []);
+
     const responsive = {
         superLargeDesktop: {
-          breakpoint: { max: 4000, min: 3000 },
-          items: 5
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
         },
         desktop: {
-          breakpoint: { max: 3000, min: 1024 },
-          items: 3
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
         },
         tablet: {
-          breakpoint: { max: 1024, min: 464 },
-          items: 2
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
         },
         mobile: {
-          breakpoint: { max: 464, min: 0 },
-          items: 1
+            breakpoint: { max: 464, min: 0 },
+            items: 1
         }
-      };
+    };
+
     return (
         <div className='Menu'>
             <div className="menuContainer">
-                <div className="tittleContainer">
-                    <h2>hola soy un listado de productos</h2>
+                <div className="titleContainerMenu">
+                    <h2>Top de Platillos</h2>
                 </div>
-                <div className="corouselContainer">
-                    <Carousel responsive={responsive}>
-                        <div>Item 1</div>
-                        <div>Item 2</div>
-                        <div>Item 3</div>
-                        <div>Item 4</div>
+                <div className="carouselContainer">
+                    <Carousel className='carousel' responsive={responsive}>
+                        {menuItems.map(item => (
+                            <MenuItem key={item.id} item={item} />
+                        ))}
                     </Carousel>
                 </div>
                 <div className="btnMenuContainer">
@@ -46,5 +57,3 @@ function ComponentName() {
 }
 
 export default ComponentName;
-
-
