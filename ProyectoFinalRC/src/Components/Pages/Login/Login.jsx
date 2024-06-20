ed02f0517344fce2469698246a52e447e6684adb
Prado Lucas Nahuel <lukasnahuelprado32@gmail.com>
2024-06-19 22:54:43 -0300

    se creo el componente de la pagina login y su hoja de estilos correspondiente

---
+++ ProyectoFinalRC/src/Components/Pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import "./Login.css"
+
+export default class Login extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hola soy un login</h1>
+      </div>
+    )
+  }
+}
