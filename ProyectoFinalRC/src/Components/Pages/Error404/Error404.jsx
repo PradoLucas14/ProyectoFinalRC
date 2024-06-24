e57b0a40cff006be8bb5b563f7e5d0d6d828175f
MatiasPerez00 <matiasperez.personal@gmail.com>
2024-06-24 10:30:35 -0300

    Creacion de efectos controladores de pantalla

--- ProyectoFinalRC/src/Components/Pages/Error404/Error404.jsx
+++ ProyectoFinalRC/src/Components/Pages/Error404/Error404.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 const Error404 = () => {
-  return (
-    <div>Error404</div>
-  )
-}
+  const [isMobile, setIsMobile] = useState(false);
 
-export default Error404
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768); 
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return (
+    <div className="error404">
+      {isMobile ? (
+        <img src="ruta-a-tu-imagen-para-móvil" alt="Error 404 - Mobile" />
+      ) : (
+        <img src="ruta-a-tu-imagen-para-escritorio" alt="Error 404 - Desktop" />
+      )}
+      <p>¡Página no encontrada!</p>
+    </div>
+  );
+};
+
+export default Error404;
