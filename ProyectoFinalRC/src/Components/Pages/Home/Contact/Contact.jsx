a99cbec16adf15bc24e3d1cab13bd86985492684
Leonel Pereyra <leonelcpereyra@gmail.com>
2024-06-26 10:52:00 -0300

    intalacion de dependencia emailJs para form de contacto

---
+++ ProyectoFinalRC/src/Components/Pages/Home/Contact/Contact.jsx
@@ -0,0 +1,20 @@
+import React, { useRef }from 'react';
+import './Contact.css';
+import emailjs from '@emailjs/browser'
+
+
+const Contact = () => {
+
+const refForm = useRef();     
+const handleSubmit = (event) => {
+    event.preventDefault();
+    
+}
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Contact
