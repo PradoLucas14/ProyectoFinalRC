f3e46042b229eb9576c14112446c348fd1000ef9
Leonel Pereyra <leonelcpereyra@gmail.com>
2024-06-27 01:26:18 -0300

    acceso a pagina contacto

--- ProyectoFinalRC/src/Components/Pages/Home/HomePage/HomePage.jsx
+++ ProyectoFinalRC/src/Components/Pages/Home/HomePage/HomePage.jsx
@@ -6,6 +6,13 @@ import Productos from '../Products/Productos';
 import { Link } from 'react-router-dom'; // Importa Link de react-router-dom
 import './HomePage.css';
 import Upload from '../../../Layout/Upload/Upload';
+import React from 'react'
+import Banner from '../Banner/Banner'
+import "./HomePage.css"
+import About from '../About/About'
+import Services from '../ServicesAndinos/Services'
+import Products from "../Products/Products"
+import Contact from '../Contact/Contact'
 
 function HomePage() {
   return (
@@ -22,6 +29,9 @@ function HomePage() {
       </div>
       <div>
       <Productos />
+      </div>
+      <div clasName='Contact'>
+      <Contact/>
       </div>
     </div>
   );
