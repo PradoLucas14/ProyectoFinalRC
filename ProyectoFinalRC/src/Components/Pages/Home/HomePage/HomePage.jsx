f9f481ca594cb2d58640089d0057214d08d12d68
Leonel Pereyra <leonelcpereyra@gmail.com>
2024-07-01 22:08:01 -0300

    modifica fondo de contacto y funcionalidad

--- ProyectoFinalRC/src/Components/Pages/Home/HomePage/HomePage.jsx
+++ ProyectoFinalRC/src/Components/Pages/Home/HomePage/HomePage.jsx
@@ -6,12 +6,7 @@ import Productos from '../Products/Productos';
 import { Link } from 'react-router-dom'; // Importa Link de react-router-dom
 import './HomePage.css';
 import Upload from '../../../Layout/Upload/Upload';
-import React from 'react'
-import Banner from '../Banner/Banner'
 import "./HomePage.css"
-import About from '../About/About'
-import Services from '../ServicesAndinos/Services'
-import Products from "../Products/Products"
 import Contact from '../Contact/Contact'
 
 
