99a12239c170f51e37bf01f1e0fa90199cd4aaa2
Leonel Pereyra <leonelcpereyra@gmail.com>
2024-07-02 19:46:12 -0300

    Se modifica banner de AboutUs

--- ProyectoFinalRC/src/Components/Pages/AboutUs/AboutUs.jsx
+++ ProyectoFinalRC/src/Components/Pages/AboutUs/AboutUs.jsx
@@ -1,22 +1,23 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import "./AboutUs.css"
 
 
 const AboutUs = () => {
   return (
-    <div className="banner">
-    <div className="overlay">
-      <Container fluid className="h-100">
+    <div className="banner-aboutus">
+    <div className="overlay-aboutus">
+      <Container fluid className="h-100 ">
           <Row className="h-100 align-items-center">
-          <Col md={6} className="text-center text-md-start d-flex flex-column justify-content-center">
+          <Col md={12} className="text-center d-flex flex-column justify-content-center">
               <div className="container1 p-1 ">
-              <h1 className="display-2">ACERCA DE NOSOTROS</h1>
+              <h1 className="display-4">ACERCA DE NOSOTROS</h1>
               
               
               </div>
           </Col>
-          <Col md={6} className="d-none d-md-block"></Col>
+          <Col md={0} className="d-none d-md-block"></Col>
           </Row>
       </Container>
     </div>
