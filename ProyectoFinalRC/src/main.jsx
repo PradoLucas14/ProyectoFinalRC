c81a5e8c0da0fea1cfdb625fe71be6283a8c6124
MatiasPerez00 <matiasperez.personal@gmail.com>
2024-07-04 02:12:07 -0300

    agregacion de animaciones de errores y succes y correccion acceso a response

--- ProyectoFinalRC/src/main.jsx
+++ ProyectoFinalRC/src/main.jsx
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { SnackbarProvider } from 'notistack'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <SnackbarProvider>
     <App />
+
+    </SnackbarProvider>
   </React.StrictMode>,
 )
