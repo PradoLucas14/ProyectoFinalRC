f32804a2b148e0609a5486aa888d5c65ef92de4b
MatiasPerez00 <matiasperez.personal@gmail.com>
2024-08-27 11:14:02 -0300

    creada rama de test de ruteado

--- ProyectoFinalRC/src/App.jsx
+++ ProyectoFinalRC/src/App.jsx
@@ -2,18 +2,11 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { jwtDecode } from 'jwt-decode';
 import './App.css';
-import HomePage from './Components/Pages/Home/HomePage/HomePage';
 import Navbar from './Components/Layout/Header/NavBar/NavBar';
 import Footer from './Components/Layout/Footer/Footer';
-import Contact from "./Components/Pages/Contact/Contact";
-import Admin from './Components/Pages/Admin/Admin';
-import RegisterUser from './Components/Pages/RegisterUser/RegisterUser';
-import Gallery from './Components/Pages/ImgGalery/ImgGalery';
-import Error404 from './Components/Pages/Error404/Error404';
-import Login from './Components/Pages/Login/Login';
-import Reservas from './Components/Pages/Reservas/Reservas';
-import AboutTeam from './Components/Pages/About/Aboutteam';
-import PrivateRoute from './PrivateRoute';
+
+import PrivateRouter from './routes/PrivateRouter';
+import PublicRouter from './routes/PublicRouter';
 
 function App() {
   const [user, setUser] = useState({
@@ -44,27 +37,11 @@ function App() {
 
   return (
     <Router>
-      {console.log(user)}
       <div className="App">
         <Navbar user={user} setUser={setUser} />
-        <Routes>
-          <Route path="/" element={<HomePage user={user} />} />
-          <Route path="/registro" element={<RegisterUser />} />
-          <Route path="/aboutteam" element={<AboutTeam />} />
-          <Route path="/login" element={<Login setUser={setUser} />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/imggalery" element={<Gallery />} />
-          <Route path="/reserve" element={<Reservas />} />
-          <Route 
-            path="/admin" 
-            element={
-              <PrivateRoute isAllowed={user.isLoggedIn} isAdmin={user.role === "Administrador"}>
-                <Admin />
-              </PrivateRoute>
-            } 
-          />
-          <Route path="*" element={<Error404 />} />
-        </Routes>
+        {
+          user.isLoggedIn?<PrivateRouter user={user} setUser={setUser}/>:<PublicRouter user={user} setUser={setUser}/>
+        }
         <Footer />
       </div>
     </Router>
